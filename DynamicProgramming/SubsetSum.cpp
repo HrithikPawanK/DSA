65c994a0e89095f52fc51b2c914bc1b09c255e5e
Your Name <you@example.com>
2021-08-16 14:30:01 +0530

    ...

--- DynamicProgramming/SubsetSum.cpp
+++ DynamicProgramming/SubsetSum.cpp
@@ -8,6 +8,26 @@ int subsetSum(int arr[], int n, int sum){
     }
     return subsetSum(arr, n - 1, sum) + subsetSum(arr, n - 1, sum - arr[n - 1]);
 }
+int subsetSumDP(int arr[], int n, int sum){
+    int dp[n+1][sum+1];
+    // note dp[0][0] = 1;
+    for(int i = 0; i <= n; i++){
+        dp[i][0] = 1;
+    }
+    for(int j = 1; j <= sum; j++){
+        dp[0][j] = 0;
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= sum; j++){
+            if(j < arr[i-1]){
+                dp[i][j] = dp[i - 1][j];
+            }else{
+                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - arr[i - 1]];
+            }
+        }
+    }
+    return dp[n][sum];
+}
 int main(){
     
     return 0;
